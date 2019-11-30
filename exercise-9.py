cd90ee396efef478fde9a3879e0d4274efab3437
Ana Anjos <anjosanapaula@outlook.com>
2019-11-30 20:20:21 -0300

    add exercise 8 and 9.

---
+++ exercise-9.py
@@ -0,0 +1,5 @@
+#Faça um algoritmo para receber um número de celular, e dê a resposta para saber é válido ou não.
+#O número deverá começar com 9;
+#O número deverá conter 9 dígitos;
+#Se for diferente de qualquer regra acima, o número está incorreto
+
