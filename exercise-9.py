f856f60e7383a96801ade6b6e089040d4050f670
Ana Anjos <anjosanapaula@outlook.com>
2019-12-01 02:24:26 -0300

    ## Faça um algoritmo para receber um número de celular ## dê a resposta para saber é válido ou não; ## o número deverá começar com 9; ## o número deverá conter 9 dígitos; ## se for diferente de qualquer regra acima, o número está incorreto.

--- exercise-9.py
+++ exercise-9.py
@@ -1,19 +1,25 @@
-#Faça um algoritmo para receber um número de celular, e dê a resposta para saber é válido ou não.
-#O número deverá começar com 9;
-#O número deverá conter 9 dígitos;
-#Se for diferente de qualquer regra acima, o número está incorreto
+import re
 
-class Celular(self, digito, celular):
-    self.digito = digito
-    self.celular = celular
+number = input("Digite o número do celular: ")
+
+## Para validar se possui 9 digitos
+def digito(digito):
+    digito = re.findall("9", number)
+
+    if (digito):
+        print("Número válido! :)")
+    else:
+        print("Número inválido! :(")
+
+## Para validar se o numero comeca com 9
+def number(number):
+    x = re.search("^9", number)
     
-    def digito(self, celular):
-        celular = int(input("Digite o número do seu celular: ")
-        if len(str(abs(celular) == 9:
-            print("Número válido :)")
-        else:
-        print("Número inválido! :()")
+    if (x):
+        print("Número válido! :)")
+    else:
+        print("Número inválido! :(")
+    
+resultado = number(number) + digito(digito)
 
-#
-
-#print(numero_celular)
+print(resultado)
